27fa6b1027954ba56e1946f4adf4e34645911d56
curowe <curowe@qq.com>
2015-12-27 14:56:24 +0800

    显示hello界面

--- app/src/main/java/me/rowe/demos/HelloActivity.java
+++ app/src/main/java/me/rowe/demos/HelloActivity.java
@@ -1,27 +1,31 @@
 package me.rowe.demos;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Rowe on 2015/12/27.
  */
-public class MainActivity extends Activity implements View.OnClickListener {
+public class HelloActivity extends Activity implements View.OnClickListener {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.button1).setOnClickListener(this);
+        setContentView(R.layout.activity_hello);
+        findViewById(R.id.button).setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.text_hello);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button1:
-                startActivity(new Intent(this, HelloActivity.class));
+            case R.id.button:
+                mTextView.setText("Hello");
                 break;
         }
     }
